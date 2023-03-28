93d10f0da55ed7ea93be25aedae34fada379fa55
bgiurgiu7@gmail.com <bgiurgiu7@gmail.com>
2023-03-28 15:00:06 -0700

    Completed Ecercise

--- app.js
+++ app.js
@@ -4,7 +4,39 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Brandon";
+let lastName;
+var age;
 
 // Exercise 2
+//I did the firstName up in Exercise 1
+lastName = "Giurgiu";
+age = 29;
+
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensative = true;
+
+console.log(`${language} was first released in ${createdYear}`);
+
+//Exercise 4
+let product = "Shirt";
+let price = 19.99
+let isOnSale = false
+let salePercetage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(`${product} in size ${selectedSize} is $${price} with ${stock} in stock.`);
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`${title} by ${author} is ${pageCount} pages long.`);
